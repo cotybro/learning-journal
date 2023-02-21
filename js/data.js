dc6ae993e2bbc595cf18c6935e4979d9d0f16edc
cotybro <36529826+cotybro@users.noreply.github.com>
2023-02-21 01:59:52 -0600

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,44 @@
+export const data = [
+    {
+        blogDate: 'july 24, 2022',
+        blogTitle: 'Blog One',
+        blogLinkL: "#",
+        blogDescription: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        blogImage: "https://images.unsplash.com/photo-1547658719-da2b51169166?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+    },
+    {
+        blogDate: 'july 25, 2022',
+        blogTitle: 'Blog Two',
+        blogLinkL: "#",
+        blogDescription: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        blogImage: 'https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60'
+    },
+    {
+        blogDate: 'july 26, 2022',
+        blogTitle: 'Blog Three',
+        blogLinkL: "#",
+        blogDescription: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        blogImage: 'https://images.unsplash.com/photo-1630659509436-7397fbda30e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60'
+    },
+    {
+        blogDate: 'july 27, 2022',
+        blogTitle: 'Blog Four',
+        blogLinkL: "#",
+        blogDescription: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        blogImage: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60'
+    },
+    {
+        blogDate: 'july 28, 2022',
+        blogTitle: 'Blog Five',
+        blogLinkL: "#",
+        blogDescription: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        blogImage: 'https://images.unsplash.com/photo-1506526794364-ba711a0d97fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTZ8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60'
+    },
+    {
+        blogDate: 'july 29, 2022',
+        blogTitle: 'Blog Six',
+        blogLinkL: "#",
+        blogDescription: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        blogImage: 'https://images.unsplash.com/photo-1607799131608-9a22dd60e25e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA4fHx3ZWIlMjBkZXZlbG9wbWVudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60'
+    },
+]
